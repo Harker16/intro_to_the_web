1732511cf5dc59d55c9dad8692b3f8c0fa283601
Thomas Harker <thomasharker@Thomass-MacBook-Pro.local>
2016-06-07 15:31:05 +0100

    Add random names

--- app.rb
+++ app.rb
@@ -1,7 +1,5 @@
 require 'sinatra'
 
 get '/heisenberg' do
-	"<div style='border: 3px dashed red; width:50%'>
-	 <img style=width:100% src ='http://media3.s-nbcnews.com/i/streams/2013/August/130808/6C8558749-130808-walter-white-tease.jpg'>
-	 </div>"
+	erb(:index)
 end
