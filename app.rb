4002f6aa1f51904bb837bde74bf7aa581a79ebb9
Thomas Harker <thomasharker@Thomass-MacBook-Pro.local>
2016-06-07 16:03:30 +0100

    Add params

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 
-get '/heisenberg' do
+get '/random-heisenberg' do
 	@name = %w(Amigo Walter Heisenberg).sample
+	erb(:index)
+end
+
+get '/named-heisenberg' do
+	p params
+	@name = params[:name]
 	erb(:index)
 end
