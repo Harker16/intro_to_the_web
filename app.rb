7ce40caffea9191658480cd0e55f2fd26ddfd78b
Thomas Harker <thomasharker@Thomass-MacBook-Pro.local>
2016-06-07 15:01:09 +0100

    Add picture border

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 
-get '/cat' do
-	"<div><img src ='http://bit.ly/1eze8aE'></div>"
+get '/heisenberg' do
+	"<div style='border: 3px dashed red; width:50%'>
+	 <img style=width:100% src ='http://media3.s-nbcnews.com/i/streams/2013/August/130808/6C8558749-130808-walter-white-tease.jpg'>
+	 </div>"
 end
